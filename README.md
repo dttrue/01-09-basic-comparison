0b8a7368b0a9246af49160b27f974f734cd21a52
Steven Zhang <steven@pursuit.org>
2023-12-06 15:31:41 -0500

    Initial commit with README and .gitignore

---
+++ README.md
@@ -0,0 +1,17 @@
+# Basic Comparison
+
+## Description
+Practice using basic comparison operators in JavaScript.
+
+## Objective
+- Compare two numbers using '>', '<', and '==' operators.
+- Console log whether each comparison is true or false.
+
+## Setup
+1. Fork and clone the repository.
+2. Navigate to the `01-09-basic-comparison` directory.
+3. Fill in the `index.js` according to the instructions.
+
+## Test Cases
+- Correctly compare the given numbers using each operator.
+- Console log should indicate whether each comparison is true or false.
---
+++ index.js
@@ -0,0 +1,17 @@
+// Given numbers
+const number1 = 10;
+const number2 = 15;
+
+// TODO: Compare if number1 is greater than number2.
+let isGreater; // Replace this line with your code.
+
+// TODO: Compare if number1 is less than number2.
+let isLess; // Replace this line with your code.
+
+// TODO: Compare if number1 is equal to number2.
+let isEqual; // Replace this line with your code.
+
+// Log Comparison Results
+console.log(`Is number1 greater than number2: ${isGreater}`);
+console.log(`Is number1 less than number2: ${isLess}`);
+console.log(`Is number1 equal to number2: ${isEqual}`);
